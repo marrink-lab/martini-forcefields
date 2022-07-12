2ae887d010cc9792136d42decf9356b1a4f63249
fgrunewald <f.grunewald@rug.nl>
2022-07-12 14:49:29 +0200

    python infra

---
+++ setup.py
@@ -0,0 +1,47 @@
+# Copyright 2022 University of Groningen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import os.path
+from setuptools import find_packages, setup
+import codecs
+# this comes form here https://packaging.python.org/guides/single-sourcing-package-version/
+# here we get the version from pbr
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+# here we collect all package data files and expose them
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+setup(
+    packages=['martini_forcefields'],
+    package_data={'': package_files('martini_forcefields'), },
+    pbr=True,
+    version=get_version("martini_forcefields/__init__.py"),
+    )
